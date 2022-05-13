552b93d03a6447574224135f6689bf0fc6542a6b
bighappyface <anomalous20@gmail.com>
2022-05-13 11:49:36 -0400

    Pivot to dark mode for light text and inputs

--- src/Theme.ts
+++ src/Theme.ts
@@ -5,14 +5,12 @@ export default createTheme({
     fontFamily: 'MaisonNeue, sans-serif',
   },
   palette: {
+    mode: 'dark',
     primary: {
       main: '#9027F7',
     },
     background: {
       default: '#0A0A0A',
-    },
-    text: {
-      primary: '#FFFFFF',
     },
   },
 })
