ae2996f5246611770e61ba55d2e39b2202fcc573
bighappyface <anomalous20@gmail.com>
2022-05-13 15:43:33 -0400

    Relocate axe expectation to setupTests.ts

--- src/a11y.test.tsx
+++ src/a11y.test.tsx
@@ -1,11 +1,9 @@
 import { render } from '@testing-library/react'
-import { axe, toHaveNoViolations } from 'jest-axe'
+import { axe } from 'jest-axe'
 import App from './App'
 
-expect.extend(toHaveNoViolations);
-
 it('should pass basic a11y checks', async () => {
-  const { container } = render(<App/>)
+  const { container } = render(<App />)
   const results = await axe(container)
   expect(results).toHaveNoViolations()
 })
--- src/components/BusinessInfoForm.test.tsx
+++ src/components/BusinessInfoForm.test.tsx
@@ -1,15 +1,12 @@
 import { render, screen } from '@testing-library/react'
-import { axe, toHaveNoViolations } from 'jest-axe'
+import { axe } from 'jest-axe'
 import BusinessInfoForm from './BusinessInfoForm'
 
-expect.extend(toHaveNoViolations);
-
 it('should pass basic a11y checks', async () => {
-  const { container } = render(<BusinessInfoForm/>)
+  const { container } = render(<BusinessInfoForm />)
   const results = await axe(container)
   expect(results).toHaveNoViolations()
 })
-
 
 test('renders the business information form', () => {
   render(<BusinessInfoForm />)
